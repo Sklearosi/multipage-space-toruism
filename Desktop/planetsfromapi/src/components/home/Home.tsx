c625e065ec8e838cfbccdd6b64ebfd54fc308dea
Sklearosi <giorgi.chanturidze.2000@gmail.com>
2023-09-25 18:40:37 +0400

    fix: files fix

--- Desktop/planetsfromapi/src/components/home/Home.tsx
+++ Desktop/planetsfromapi/src/components/home/Home.tsx
@@ -3,7 +3,7 @@
 const Home = () => {
     return(
         <div>
-            <div className=" w-11/12 m-auto mt-10 ">
+            <div className=" w-11/12 m-auto mt-14 ">
                 <p className=" font-[400] text-[#D0D6F9] text-[16px] text-center font-[Barlow condensed] ">SO, YOU WANT TO TRAVEL TO</p>
             </div>
         </div>
